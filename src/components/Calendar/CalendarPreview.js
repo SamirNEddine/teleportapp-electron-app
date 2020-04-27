963807a45b156463a6adcc123c1e419e72f5dbff
Samir <samir@nevereatalone.io>
2020-04-27 16:53:23 +0200

    positioning hours

--- src/components/Calendar/CalendarPreview.js
+++ src/components/Calendar/CalendarPreview.js
@@ -10,7 +10,7 @@ const CalendarPreview = function ({startTime, endTime}) {
             const split1 = time.split(':');
             const split2 = time.split(' ');
             console.log(split2);
-            const hour = split1[0] + (split2[1] ? ` ${split2[1]}` : '');
+            const hour = split1[0] + (split2[1] ? ` ${split2[1]}` : `:${split1[2]}`);
             hoursRows.push(<li key={hour}>{hour}</li>)
         }
         return hoursRows;
