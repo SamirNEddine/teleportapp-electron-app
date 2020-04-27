15d97e39eef06e0a372408a57aadd8758995357e
Samir <samir@nevereatalone.io>
2020-04-27 17:40:20 +0200

    use in code styling

--- src/components/Calendar/CalendarPreview.js
+++ src/components/Calendar/CalendarPreview.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import './calendar.css';
 
+const styles = {
+    container: {
+        position: 'relative',
+        width: '100%',
+        height: '100%'
+    },
+    hoursList: {
+        position: 'relative',
+        padding: 0,
+        margin: 0,
+        textAlign: 'right',
+        top: '48px',
+        left: '14px',
+        width: '28px',
+        listStyle: 'none',
+        userSelect: 'none'
+    },
+    hoursListLi: {
+        fontFamily: 'Silka',
+        fontSize: '9px',
+        fontWeight: 300,
+        fontStretch: 'normal',
+        fontStyle: 'normal',
+        lineHeight: 'normal',
+        letterSpacing: 'normal',
+        color: 'var(--blueberry)',
+        paddingBottom: '22px'
+    }
+};
+
+
 const CalendarPreview = function ({startTime, endTime}) {
 
     const renderCalendarRows = function () {
@@ -11,15 +42,15 @@ const CalendarPreview = function ({startTime, endTime}) {
             const split2 = time.split(' ');
             console.log(split2);
             const hour = split1[0] + (split2[1] ? ` ${split2[1]}` : `:${split1[2]}`);
-            hoursRows.push(<li key={hour}>{hour}</li>)
+            hoursRows.push(<li style={styles.hoursListLi} key={hour}>{hour}</li>)
         }
         return hoursRows;
     };
 
 
     return (
-        <div className='calendar-preview-container'>
-            <ul className='hours-list'>
+        <div style={styles.container}>
+            <ul style={styles.hoursList}>
                 {renderCalendarRows()}
             </ul>
         </div>
---
+++ src/components/Calendar/TimeSlot.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const styles = {
+    container: {
+        backgroundColor: 'white',
+    },
+};
+
+const CalendarPreview = function ({startTime, endTime, status}) {
+    return (
+        <div >
+        </div>
+    )
+};
+
+export default CalendarPreview;
