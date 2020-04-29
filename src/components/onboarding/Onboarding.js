b4dfd394fd2928f4899cc3bdafcd4fb633db41b8
Samir <samir@nevereatalone.io>
2020-04-29 18:53:43 +0200

    animation support

--- src/components/onboarding/Onboarding.js
+++ src/components/onboarding/Onboarding.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect, useCallback} from 'react';
 import UserProfile from "./UserProfile";
 import AvailabilityProfile from './AvailabilityProfile';
 import CalendarIntegration from "./CalendarIntegration";
-import Reveal from 'react-reveal/Reveal'
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import './onboarding.css'
 import '../../assets/animate.css';
 
+const TRANSITION_SPEED = 800;
 const Onboarding = function () {
     const [currentScreen, setCurrentScreen] = useState(null);
 
@@ -14,12 +15,20 @@ const Onboarding = function () {
             switch (screen) {
                 case 'UserProfile':
                 {
-                    setCurrentScreen(<AvailabilityProfile onConfirmButtonClick={ () => {onConfirmButtonClick('AvailabilityProfile')}}/>);
+                    setCurrentScreen(
+                        <CSSTransition key='AvailabilityProfile' timeout={TRANSITION_SPEED} classNames="slide">
+                            <AvailabilityProfile onConfirmButtonClick={ () => {onConfirmButtonClick('AvailabilityProfile')}}/>
+                        </CSSTransition>
+                    );
                     break;
                 }
                 case 'AvailabilityProfile':
                 {
-                    setCurrentScreen(<CalendarIntegration onConfirmButtonClick={ () => {onConfirmButtonClick('CalendarIntegration')}}/>);
+                    setCurrentScreen(
+                        <CSSTransition key='CalendarIntegration' timeout={TRANSITION_SPEED} classNames="slide">
+                            <CalendarIntegration onConfirmButtonClick={ () => {onConfirmButtonClick('CalendarIntegration')}}/>
+                        </CSSTransition>
+                    );
                     break;
                 }
                 case 'CalendarIntegration':
@@ -33,13 +42,17 @@ const Onboarding = function () {
     );
 
     useEffect( () => {
-        setCurrentScreen(<UserProfile onConfirmButtonClick={ () => {onConfirmButtonClick('UserProfile')}}/>);
+        setCurrentScreen(
+            <CSSTransition key='UserProfile' timeout={TRANSITION_SPEED}  classNames={{exit: 'slide-exit', exitActive: 'slide-exit-active'}}>
+                <UserProfile onConfirmButtonClick={ () => {onConfirmButtonClick('UserProfile')}}/>
+            </CSSTransition>
+        );
     }, []);
 
     return (
-        <div className='onboarding-container'>
+        <TransitionGroup className='onboarding-container'>
             {currentScreen}
-        </div>
+        </TransitionGroup>
     );
 };
 
