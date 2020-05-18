8ab29c245fec1a8609922e6d2f377d1466128e77
Samir <samir@nevereatalone.io>
2020-05-18 15:59:25 +0200

    add sentry

--- public/preload.js
+++ public/preload.js
@@ -1,1 +1,3 @@
 window.remote = require('electron').remote;
+const Sentry = require('@sentry/electron');
+Sentry.init({dsn: 'https://20e5500288824dedb0176c782d1ac9c4@o304110.ingest.sentry.io/5244767'});
